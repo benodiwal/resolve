facdbc6e459c1004797709ba091027e2ad8c7aeb
codygeek01 <s474996633@gmail.com>
2024-04-27 16:31:35 +0530

    added custom dns server support

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use resolver::resolve;
 fn main() {
     let args = Args::parse_args();
     let domain_name = args.domain_name.unwrap();
+    let dns_server = args.dns_server.as_deref();
 
-    resolve(&domain_name);
+    resolve(&domain_name, dns_server);
 }
--- src/resolver/mod.rs
+++ src/resolver/mod.rs
@@ -1,7 +1,26 @@
-use trust_dns_resolver::Resolver;
+use std::net::SocketAddr;
 
-pub fn resolve(domain: &str) {
-    let resolver = Resolver::default().unwrap();
+use trust_dns_resolver::{config::ResolverConfig, Resolver};
+use trust_dns_resolver::config::{NameServerConfig, Protocol, ResolverOpts};
+
+pub fn resolve(domain: &str, dns_server: Option<&str>) {
+    let resolver_config = match dns_server {
+        Some(ip_str) => {
+            
+            let ip = ip_str.parse().unwrap_or_else(|err| {
+                eprintln!("Error parsing IP address: {}", err);
+                std::process::exit(1);
+            });
+
+            let custom_dns_server = NameServerConfig::new(SocketAddr::new(ip, 53), Protocol::Tcp);
+            ResolverConfig::from_parts(None, Vec::new(), vec![custom_dns_server])
+        }
+
+        None => ResolverConfig::default(),
+    };
+
+    let resolver_opts = ResolverOpts::default();
+    let resolver = Resolver::new(resolver_config, resolver_opts).unwrap();
     let response = resolver.lookup_ip(domain);
 
     match response {
